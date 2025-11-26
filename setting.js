456d31357e994e0cc7c394d0f1606e1f2db83c29
aaahsisiaid <a6068376@gmail.com>
2025-11-26 20:13:43 +0900

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,18 +1,1 @@
-import { db, auth } from "./firebase.js";
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 
-document.getElementById("save").onclick = async () => {
-    const uid = auth.currentUser.uid;
-
-    await setDoc(
-        doc(db, "users", uid, "settings", "jalert"),
-        {
-            date: document.getElementById("training-date").value,
-            daysBefore: Number(document.getElementById("days-before").value),
-            notifyTime: document.getElementById("notify-time").value
-        },
-        { merge: true }
-    );
-
-    alert("保存しました！");
-};
